bdfdbc3c91dc7eda5dcc7acc438092cd4edb2c3c
Zuyyina79 <74144537+Zuyyina79@users.noreply.github.com>
2020-11-08 21:55:43 +0700

    Add files via upload

---
+++ Program Jumlah & Kuadrat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int a,b,jumlah,kuadrat;
+        printf("Masukkan a =",&a);
+        scanf("%i",&a);
+        printf("Masukkan b =",&b);
+        scanf("%i",&b);
+        jumlah=a+b;
+        kuadrat=jumlah*jumlah;
+        printf("\n hasil jumlah :%i",jumlah);
+
+        printf("\n hasil kuadrat :%i",kuadrat);
+
+    return 0;
+}
