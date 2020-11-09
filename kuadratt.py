96157407ad1d2106210b19ab6c9c84ec8d1a3598
ADS43-pixel <72546517+ADS43-pixel@users.noreply.github.com>
2020-11-09 14:16:33 +0700

    Add files via upload

---
+++ kuadratt.py
@@ -0,0 +1,17 @@
+
+
+#penjumlahan
+print("=======Penjumlahan======")
+def add (a,b,):
+  return a + b
+a = int(input("Masukkan bilangan pertama: "))
+b = int(input("Masukkan bilangan kedua: "))
+print(a,"+",b,"=", add(a,b))
+
+#kuadrat
+print("=======kuadrat=======")
+def multiply (a,b):
+  return a ** b
+a =int(input("Masukkan Bilangan Pertama:"))
+b =int(input("Masukkan Bilangan Kedua:"))
+print(a**b)
