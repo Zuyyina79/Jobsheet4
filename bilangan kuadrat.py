8ed97a7cccd73106e11c535c228b11a7ba569504
ADS43-pixel <72546517+ADS43-pixel@users.noreply.github.com>
2020-11-10 22:37:30 +0700

    Delete bilangan kuadrat.py

--- bilangan kuadrat.py
+++
@@ -1,8 +0,0 @@
-
-#kuadrat
-print("=======kuadrat=======")
-def multiply (a,b):
-  return a ** b
-a =int(input("Masukkan Bilangan Pertama:"))
-b =int(input("Masukkan Bilangan Kedua:"))
-print(a*b*c)
